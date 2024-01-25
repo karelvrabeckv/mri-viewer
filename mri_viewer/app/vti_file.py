d6ba67ecffd00a7622d1e946804c2e960983f992
Karel Vrabec <karelvrabeckv@gmail.com>
2024-01-25 15:14:19 +0100

    File management IMPROVED

---
+++ mri_viewer/app/vti_file.py
@@ -0,0 +1,40 @@
+class VTIFile:
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter 
+    def name(self, name): 
+        self._name = name
+
+    @property
+    def reader(self):
+        return self._reader
+
+    @reader.setter 
+    def reader(self, reader): 
+        self._reader = reader
+
+    @property
+    def data(self):
+        return self._data 
+
+    @data.setter 
+    def data(self, data): 
+        self._data = data
+
+    @property
+    def active_array(self):
+        return self._active_array
+
+    @active_array.setter 
+    def active_array(self, active_array): 
+        self._active_array = active_array
+
+    @property
+    def data_arrays(self):
+        return self._data_arrays
+    
+    @data_arrays.setter 
+    def data_arrays(self, data_arrays): 
+        self._data_arrays = data_arrays 
